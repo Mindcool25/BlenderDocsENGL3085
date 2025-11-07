f1c40e6560319091333ec78dda5c491bb21fdc28
mindcool24 <mindcool@mindcool.xyz>
2025-11-07 15:18:04 -0700

    rundoc updated

--- rundoc.sh
+++ rundoc.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-pandoc docs/*.md -o out/BlenderDocs.pdf --toc -s
-
+pandoc docs/*.md -o out/pages.pdf --toc -s
+pdfjam --paper a4 docs/Blender\ for\ Beginners.pdf -o out/title.pdf
+pdfunite  out/title.pdf out/pages.pdf out/BlenderDocs.pdf
